5758352087e0a973c1aaa7ede4a3a0d549aaf52a
Elsa Yang <elsayang0503@gmail.com>
2023-03-30 14:48:33 +0800

    added try and except

--- scaper.py
+++ scaper.py
@@ -11,40 +11,45 @@ headers = {
 }
 
 url = 'https://www.bbc.com/zhongwen/trad'
-response_bbc = requests.get(url=url, headers=headers, timeout =5)
 
-if response_bbc.status_code ==200:
-    soup_bbc = bs(response_bbc.text, 'lxml')
+try:
+    response_bbc = requests.get(url=url, headers=headers, timeout =5)
 
-    news_all = []
-    urls = soup_bbc.find_all('h3', {'class':'bbc-189hdql ea6by782'})
-    for i in urls:
-        news = {}
-        tag = [] 
-        time.sleep(1)
-        subresponse = requests.get('https://www.bbc.com' + i.a["href"], headers=headers)
-        soup_sub = bs(subresponse.text, 'lxml')
-        titles_article = soup_sub.find('h1', id='content')
+    if response_bbc.status_code ==200:
+        soup_bbc = bs(response_bbc.text, 'lxml')
 
-        if titles_article: 
-            news['title'] = titles_article.text
-        else:
-            print("title not existed")
+        news_all = []
+        urls = soup_bbc.find_all('h3', {'class':'bbc-189hdql ea6by782'})
+        for i in urls:
+            news = {}
+            tag = [] 
+            time.sleep(1)
+            subresponse = requests.get('https://www.bbc.com' + i.a["href"], headers=headers)
+            soup_sub = bs(subresponse.text, 'lxml')
+            titles_article = soup_sub.find('h1', id='content')
 
-        tags = soup_sub.find_all('li', 'bbc-1msyfg1 e2o6ii40')
+            if titles_article: 
+                news['title'] = titles_article.text
+            else:
+                print("title not existed")
 
-        if tags:
-            for elements in tags:
-                tag.append(elements.text)
-        else:
-            print("tags not existed")
+            tags = soup_sub.find_all('li', 'bbc-1msyfg1 e2o6ii40')
 
-        news['tags']= tag
-        print(news)
-        news_all.append(news)
+            if tags:
+                for elements in tags:
+                    tag.append(elements.text)
+            else:
+                print("tags not existed")
 
-else:
-    print("status_code is not 200")
+            news['tags']= tag
+            print(news)
+            news_all.append(news)
 
-with open ('bbc_news.json', 'w') as f:
-        json.dump(news_all, f, indent=4, ensure_ascii=False)
+    else:
+        print("status_code is not 200")
+
+    with open ('bbc_news.json', 'w') as f:
+            json.dump(news_all, f, indent=4, ensure_ascii=False)
+
+except Exception as e:
+    print(str(e))
