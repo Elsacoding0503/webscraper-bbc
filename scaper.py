92b156219b98ccd9ecb38d14c8e61db2674e5b30
Elsa Yang <elsayang0503@gmail.com>
2023-03-30 14:44:54 +0800

    updated timeout and json

--- scaper.py
+++ scaper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import  BeautifulSoup as bs
 import time
+import json
 from fake_useragent import UserAgent 
 
 ua = UserAgent()
@@ -10,23 +11,40 @@ headers = {
 }
 
 url = 'https://www.bbc.com/zhongwen/trad'
-response_bbc = requests.get(url=url)
-soup_bbc = bs(response_bbc.text, 'lxml')
+response_bbc = requests.get(url=url, headers=headers, timeout =5)
 
-news_all = []
-urls = soup_bbc.find_all('h3', {'class':'bbc-189hdql ea6by782'})
-for i in urls:
-    news = {}
-    tag = [] 
-    time.sleep(1)
-    subresponse = requests.get('https://www.bbc.com' + i.a["href"], headers=headers)
-    soup_sub = bs(subresponse.text, 'lxml')
-    titles_article = soup_sub.find('h1', id='content')
-    news['title'] = titles_article.text
-    tags = soup_sub.find_all('li', 'bbc-1msyfg1 e2o6ii40')
+if response_bbc.status_code ==200:
+    soup_bbc = bs(response_bbc.text, 'lxml')
 
-    for elements in tags:
-        tag.append(elements.text)
-    news['tags']= tag
-    print(news)
-    news_all.append(news)
+    news_all = []
+    urls = soup_bbc.find_all('h3', {'class':'bbc-189hdql ea6by782'})
+    for i in urls:
+        news = {}
+        tag = [] 
+        time.sleep(1)
+        subresponse = requests.get('https://www.bbc.com' + i.a["href"], headers=headers)
+        soup_sub = bs(subresponse.text, 'lxml')
+        titles_article = soup_sub.find('h1', id='content')
+
+        if titles_article: 
+            news['title'] = titles_article.text
+        else:
+            print("title not existed")
+
+        tags = soup_sub.find_all('li', 'bbc-1msyfg1 e2o6ii40')
+
+        if tags:
+            for elements in tags:
+                tag.append(elements.text)
+        else:
+            print("tags not existed")
+
+        news['tags']= tag
+        print(news)
+        news_all.append(news)
+
+else:
+    print("status_code is not 200")
+
+with open ('bbc_news.json', 'w') as f:
+        json.dump(news_all, f, indent=4, ensure_ascii=False)
